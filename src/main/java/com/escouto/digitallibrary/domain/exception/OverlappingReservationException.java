4f5ca43b67b997add90144bb59e7d4bf7ac6450f
arionildo_escouto <arionildo_escouto@sicredi.com.br>
2024-04-22 19:18:54 -0300

    Fix Status Error and Payload Validations

--- src/main/java/com/escouto/digitallibrary/domain/exception/OverlappingReservationException.java
+++ src/main/java/com/escouto/digitallibrary/domain/exception/OverlappingReservationException.java
@@ -1,5 +1,9 @@
 package com.escouto.digitallibrary.domain.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class OverlappingReservationException extends RuntimeException {
 
     public OverlappingReservationException(String message) {
--- src/main/resources/db/migration/V1.4.6__insert_reviews.sql
+++ src/main/resources/db/migration/V1.4.6__insert_reviews.sql
@@ -2,8 +2,8 @@ INSERT INTO REVIEW (user_id, book_id, rating, comment, created_at)
 SELECT
     user_id,
     book_id,
-    FLOOR(RAND() * 5) + 1 AS rating,
-    CASE WHEN FLOOR(RAND() * 2) = 1 THEN 'Good book' ELSE 'Bad book' END AS comment,
+    FLOOR(RAND() * 10) + 1 AS rating,
+    CASE WHEN FLOOR(RAND() * 4) = 1 THEN 'Good book' ELSE 'Nice book' END AS comment,
     DATEADD('DAY', -FLOOR(RAND() * 365), CURRENT_TIMESTAMP) AS created_at
 FROM
     (SELECT u.id AS user_id, b.id AS book_id FROM users u CROSS JOIN Book b) AS cross_join;
